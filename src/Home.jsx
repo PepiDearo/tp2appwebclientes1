be4d7c9e673cdb6ae84da58113c1630c3bf23446
PepiDearo <112524527+PepiDearo@users.noreply.github.com>
2025-04-22 19:59:41 -0400

    changement sur home.jsx pour fonction de filtre studio dropdown egalement

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,29 +1,92 @@
 import { useState, useEffect } from 'react';
 import { TvShow } from './TvShow';
+import { StudioDropdown } from './StudioDropdown';
 
 export function Home() {
   const [tvShows, setTvShows] = useState([]);
+  const [studios,setStudios] = useState([]);
+  const [selectedStudio, setSelectedStudio] = useState("");
 
-  useEffect(() => {
+  
     async function getTvShows() {
       const rep = await fetch('https://tvshowdbapi.herokuapp.com/tvshows');
       if (rep.ok) {
-        const data = await rep.json();
-        setTvShows(data);
+        const tvshow_data = await rep.json();
+        setTvShows(tvshow_data);
       }
     };
-    getTvShows();
-  }, []);
 
-  return (
+    async function getStudios() {
+        const rep = await fetch('https://tvshowdbapi.herokuapp.com/studios');
+        if (rep.ok) {
+          const studio_data = await rep.json();
+          setStudios(studio_data);
+        }
+      };
+    
+
+
+  useEffect(() => {
+    getTvShows();
+    getStudios();
+}, []);
+
+
+
+  function showFiltre(){
+    let showFF=tvShows;
+    if (selectedStudio){
+        showFF = showFF.filter(show => show.studio.name === selectedStudio);
+    }
+    return showFF
+
+
+}
+
+
+
+
+
+
+
+
+
+
+return (
     <section className="section">
       <div className="container">
         <h1 className="title has-text-centered">TV Shows</h1>
+        
+        {/* Studio Dropdown Filter */}
+        <div className="columns is-centered">
+          <div className="column is-half">
+            <StudioDropdown 
+              studios={studios.map(studio => studio.name)} 
+              selectedStudio={selectedStudio}
+              onStudioChange={setSelectedStudio}
+            />
+          </div>
+        </div>
+
+        {/* TV Shows Grid */}
         <div className="columns is-multiline">
-          {tvShows.map((show) => {
-            return <TvShow key={show.tvshowId} tvShow={show} />})}
+          {showFiltre().map((show) => (
+            <TvShow key={show.tvshowId} tvShow={show} />
+          ))}
         </div>
       </div>
     </section>
   );
+}
+
+
+
+function showFiltre(){
+    let showFF=tvShows;
+    if (selectedStudio){
+        showFF = showFF.filter(show => show.studio.name === studios);
+    }
+    return showFF
+
+
 }
