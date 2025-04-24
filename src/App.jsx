c02d4aa9c30c133047ebd368c0f791178b1917c7
PepiDearo <112524527+PepiDearo@users.noreply.github.com>
2025-04-24 13:07:21 -0400

    Ajouter route pour saison, lorsqu'on clique sur une saison (en bas de page details)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './Home';
 import { Details } from './Details';
+import {Saison} from './Saison'
 import 'bulma/css/bulma.min.css';
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/details/:id" element={<Details />} />
+          <Route path="/saison/:seasonId" element={<Saison />} />
         </Routes>
       </div>
     </Router>
