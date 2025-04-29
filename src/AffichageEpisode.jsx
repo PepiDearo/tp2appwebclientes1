d400b98061b9a2c45408b59c8162040a78944c20
PepiDearo <112524527+PepiDearo@users.noreply.github.com>
2025-04-28 23:29:45 -0400

    Fonction pour afficher les episodes dans la page saison

---
+++ src/AffichageEpisode.jsx
@@ -0,0 +1,19 @@
+// AffichageEpisode.jsx
+export function AffichageEpisode({ episode }) {
+    return (
+      <div className="column is-3-desktop is-4-tablet is-6-mobile">
+        <div className="card">
+          <div className="card-image">
+            <figure className="image is-16by9">
+              <img src={episode.imgURL} alt={episode.title} />
+            </figure>
+          </div>
+          <div className="card-content">
+            <h3 className="title is-5">{episode.title}</h3>
+            <p>{episode.number}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
