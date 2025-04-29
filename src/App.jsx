f46516d9ab57a99e9128bfab2943e664969b447b
PepiDearo <112524527+PepiDearo@users.noreply.github.com>
2025-04-28 22:31:40 -0400

    Changer selon les parties a faire de la partie 2 du tp

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './Home';
 import { Details } from './Details';
-import {Saison} from './Saison'
+import { Saison } from './Saison';
+import { Menu } from './Menu';
+import { Signup } from './Signup';
+import { Login } from './Login';
+import { AuthProvider } from './AuthContext';
 import 'bulma/css/bulma.min.css';
-
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/details/:id" element={<Details />} />
-          <Route path="/saison/:seasonId" element={<Saison />} />
-        </Routes>
-      </div>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <div className="App">
+          <Menu />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/saison/:seasonId" element={<Saison />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
+    </AuthProvider>
   );
 }
 
 export default App;
-
-
-
